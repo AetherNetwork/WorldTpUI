09d185578968a8de1f8473e763b653010db0871a
Zero <epicsteve33@gmail.com>
2017-12-01 14:30:26 -0800

    Update wtpuiCommand.php

--- src/Zero/WorldTpUI/Command/wtpuiCommand.php
+++ src/Zero/WorldTpUI/Command/wtpuiCommand.php
@@ -6,6 +6,8 @@ namespace Zero\WorldTpUI\Command;
 
 use pocketmine\Player;
 
+use pocketmine\Server;
+
 use pocketmine\utils\TextFormat as T;
 
 use pocketmine\command\Command;
@@ -23,34 +25,33 @@ class wtpuiCommand extends VanillaCommand {
     $this->setPermission('plugins.command');
   }
 
-  public function execute(CommandSender $sender, $alias, array $args){
-  if($sender instanceof Player){
-  if($sender->isOp() === true){
-    $ui = $this->plugin->ui['world-tp'];
-    $ui->data = ['type' => 'custom_form', 'title' => 'WorldTpUI '. $this->plugin->getDescription()->getVersion(), 
-    'content' => [
-      ['type' => 'input', 'text' => 'Type a world name', 'placeholder' => 'WorldName', 'default' => null],
-      ['type' => 'step_slider', 'text' => 'load area around yourself', 'steps' => array("\n0, load none", "\nload 4x4 area", "\nload 8x8 area")],
-      ["type" => "label", "text" => "Worlds Loaded:\n". T::AQUA . $this->getLevels()]
-    ]];
-    $ui->send($sender);
+  public function execute(CommandSender $player, $alias, array $args){
+  if($player instanceof Player){
+  if($player->isOp() === true){
+    $worlds = array_map(function (\pocketmine\level\Level $level){
+      return new \pocketmine\form\MenuOption($level->getName());
+    }, Server::getInstance()->getLevels());
+    $player->sendForm(
+      new class(T::DARK_PURPLE . "Worlds", "Teleport to any world", array_values($worlds)) extends \pocketmine\form\MenuForm{
+        public function onSubmit(Player $player): ?\pocketmine\form\Form{
+          $selectedOption = $this->getSelectedOption()->getText();
+          $world = Server::getInstance()->getLevelByName($selectedOption);
+          if (!is_null($world)) $player->teleport($world->getSpawnLocation());
+          else $player->sendForm(new class("World was not found", "Please contact an administrator about this\nError: World not found\nWorldname: " . $selectedOption, "gui.yes", "gui.no") extends \pocketmine\form\ModalForm{
+          }, true);
+          return null;
+        }
+      }
+      , true
+    );
     return true;
   } else {
-    $sender->sendMessage(T::RED."You must be Op to run this Command!");
+    $player->sendMessage(T::RED."You must be Op to run this Command!");
     return false;
    }
   } else {
-    $sender->sendMessage(T::RED."Command must be run in-game!");
+    $player->sendMessage(T::RED."Command must be run in-game!");
     return false;     
    }
-  }
-
-  public function getLevels(){
-    $levels = $this->plugin->getServer()->getLevels();
-  foreach($levels as $level){
-    $lvl[$level->getName()] = $level;
-  }
-    return implode(", ", array_keys($lvl));
-    unset($lvl);
   }
 }
